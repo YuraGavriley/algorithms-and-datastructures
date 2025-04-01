b86d0aaa706129043240f3a110d13705157165c7
YuraGavriley <yura.gavriley@gmail.com>
2025-04-01 15:26:11 +0300

    feat: add pop method to LinkedList

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -29,3 +29,33 @@ class LinkedList:
             self.tail = new_node
 
         self.length += 1
+
+    def pop(self):
+        # Empty linked list
+        if self.head is None:
+            return None
+
+        # Linked list with 1 element
+        # self.head and self.tail point to the same place
+        elif self.head.next is None:
+            last_node = self.tail
+            self.head = None
+            self.tail = self.head
+            self.length -= 1
+
+            return last_node
+
+        # Linked list with more then 1 element
+        else:
+            temp_node = self.head
+            last_node = self.tail
+
+            while temp_node.next is not last_node:
+                temp_node = temp_node.next
+
+
+            self.tail = temp_node
+            self.tail.next = None
+            self.length -= 1
+
+            return last_node
