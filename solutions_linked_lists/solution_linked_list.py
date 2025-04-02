18bb2faf47e616a92ce46ecf5296b112be003890
YuraGavriley <yura.gavriley@gmail.com>
2025-04-02 11:34:50 +0300

    feat: add pop solution for LinkedList

--- solutions_linked_lists/solution_linked_list.py
+++ solutions_linked_lists/solution_linked_list.py
@@ -32,32 +32,32 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+    def pop(self):
+        if self.length == 0:
+            return None
 
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
 
 my_linked_list = LinkedList(1)
-my_linked_list.make_empty()
-
-my_linked_list.append(1)
 my_linked_list.append(2)
 
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length, '\n')
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop())
+# (1) Items - Returns 1 Node
+print(my_linked_list.pop())
+# (0) Items - Returns None
+print(my_linked_list.pop())
 
-print('Linked List:')
-my_linked_list.print_list()
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Head: 1
-    Tail: 2
-    Length: 2
-
-    Linked List:
-    1
-    2
-
-"""
