1e48976b8cdac3f226713b2cc0846593b1e5a616
YuraGavriley <yura.gavriley@gmail.com>
2025-04-11 14:41:13 +0300

    refactor(ll_leetcode): Create ll_base file and import base LinkedList class from it

--- ll_leetcode_exercises/ll_find_middle_node.py
+++ ll_leetcode_exercises/ll_find_middle_node.py
@@ -1,27 +1,7 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+from ll_base import LinkedList
 
 
-class LinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        return True
-
-
+class LinkedListFindMiddleNode(LinkedList):
     def find_middle_node(self):
         slow = self.head
         fast = self.head
@@ -32,7 +12,7 @@ class LinkedList:
 
 
 
-my_linked_list = LinkedList(1)
+my_linked_list = LinkedListFindMiddleNode(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
