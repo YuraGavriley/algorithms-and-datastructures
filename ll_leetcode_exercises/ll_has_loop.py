483cbbe5272e72b8359689edfa3c2b4553698534
YuraGavriley <yura.gavriley@gmail.com>
2025-04-11 16:01:44 +0300

    refactor(ll_leetcode): Change import and redone has_loop method cleaner

--- ll_leetcode_exercises/ll_has_loop.py
+++ ll_leetcode_exercises/ll_has_loop.py
@@ -1,17 +1,30 @@
-from ll_find_middle_node import LinkedList
+from ll_base import LinkedList
 
 class LinkedListHasLoop(LinkedList):
+    # def has_loop(self):
+    #     slow = self.head.next
+    #     fast = self.head.next.next
+    #     while fast != slow and fast != self.tail and fast is not None:
+    #         slow = slow.next
+    #         fast = fast.next.next
+
+    #     if fast == slow:
+    #         return True
+    #     return False
+
     def has_loop(self):
-        slow = self.head.next
-        fast = self.head.next.next
-        while fast != slow and fast != self.tail and fast is not None:
+        slow = self.head
+        fast = self.head
+
+        # This stuff is a tricky one. If fast.next is not None goes first in the condition,
+        # it could potentially be None.next which will throw an error.
+        # But if we check fast first (if it is None), the loop will not check the second condition and will simply break.
+        while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
-
-        if fast == slow:
-            return True
+            if fast == slow:
+                return True
         return False
-
 
 
 my_linked_list_1 = LinkedListHasLoop(1)
